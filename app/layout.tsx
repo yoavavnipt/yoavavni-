5183a2b40b11f482a98a0cc098cbcc64168c6b10
yoavavnipt <yoavavni.pt@gmail.com>
2026-05-09 20:54:50 +0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="he" dir="rtl">
+      <body style={{ margin: 0 }}>
+        {children}
+      </body>
+    </html>
+  )
+}
