6f71bf2a959e27cb040430ace2345d406d704b6b
yoavavnipt <yoavavni.pt@gmail.com>
2026-05-10 11:36:05 +0300

    Delete app directory

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="he" dir="rtl">
-      <body style={{ margin: 0 }}>
-        {children}
-      </body>
-    </html>
-  )
-}
